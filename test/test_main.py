811ce92c91e3264320ab687796e8bef9768e7494
fullstacksetup <fullstacksetup@gmail.com>
2025-10-26 23:11:46 -0400

    test sub added

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,11 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add():
     assert add(1,2) == 3
     assert add(3,4) == 7
     assert add(0,0) == 0
+
+def test_subtract():
+    assert subtract(2,2) == 0
+    assert subtract(5,4) == 1
+    assert subtract(1,0) == 1
